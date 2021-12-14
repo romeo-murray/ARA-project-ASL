ea867a2971780cc6dfad4b2779a779b52bf95aea
romeo-murray <romeomurray8@gmail.com>
2021-12-13 20:07:47 -0800

    Update asl_create_dataset.py

--- asl_create_dataset.py
+++ asl_create_dataset.py
@@ -6,7 +6,7 @@ import random
 import pickle
 
 IMG_SIZE = 50 # size of the images (for compression)
-DATADIR = 'C:/dev/ARA-asl-project/asl_alphabet_train/'
+DATADIR = 'C:/your/directory/asl_alphabet_train/' # place the directory path for the asl_training_images here
 CATEGORIES = ['A', 'B', 'C', 'D', 'del', 'E', 'F', 'G', 'H', 'I', 
              'J', 'K', 'L', 'M', 'N', 'nothing', 'O', 'P', 'Q', 'R', 
              'S', 'space', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
